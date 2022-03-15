d961b32440ce3ff6f0d3cff52ed4734ec4bbdc52
clau-nunes <claudiarobertapsi@gmail.com>
2022-03-15 18:07:47 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using static System.Console;
+
+ Pessoa p1 = new Pessoa();
+
+p1.Nome = "Cláudia";
+p1.Idade = 24;
+
+p1.EnderecoPessoa = new Endereco()
+ {
+     Logradouro = "Rua teste",
+     Numero = 300,
+     CEP = "000000",
+     Cidade = "Paraiba",
+
+ }; 
+
+ WriteLine("Fim");
+ 
